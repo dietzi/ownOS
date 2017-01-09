3e549261ba0047c0466ee295d979697d32c36df4
dietzi <info@madi-services.de>
2017-01-09 19:17:57 +0100

    Changed source

--- boot/idt.h
+++ boot/idt.h
@@ -7,25 +7,24 @@
 
 #include "mem.h"
 
-/* Defines an IDT entry */
-struct idt_entry{
-    uint16_t base_lo;
-    uint16_t sel;        /* Our kernel segment goes here! */
-    uint8_t always0;     /* This will ALWAYS be set to 0! */
-    uint8_t flags;       /* Set using the above table! */
-    uint16_t base_hi;
-} __attribute__((packed));
-
-struct idt_ptr{
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed));
-
-/* This exists in 'start.asm', and is used to load our IDT */
-extern void _idt_load();
-
-void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
-
-void idt_install();
-
+struct cpu_state {
+    // Von Hand gesicherte Register
+    uint32_t   eax;
+    uint32_t   ebx;
+    uint32_t   ecx;
+    uint32_t   edx;
+    uint32_t   esi;
+    uint32_t   edi;
+    uint32_t   ebp;
+ 
+    uint32_t   intr;
+    uint32_t   error;
+ 
+    // Von der CPU gesichert
+    uint32_t   eip;
+    uint32_t   cs;
+    uint32_t   eflags;
+    uint32_t   esp;
+    uint32_t   ss;
+};
 #endif
