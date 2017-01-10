9a5037e3ed8464e5a37594c7eb5dd9c789e51662
dietzi <info@madi-services.de>
2017-01-10 12:58:55 +0100

    Code changed

--- boot/idt.c
+++ boot/idt.c
@@ -3,26 +3,3 @@
 #include <stdint.h>
 #include "idt.h"
 
-void handle_interrupt(struct cpu_state* cpu)
-{
-    if (cpu->intr <= 0x1f) {
-        terminal_writestring("Exception!");
-		//terminal_writestring((char*)cpu->intr);
- 
-        // Hier den CPU-Zustand ausgeben
- 
-        while(1) {
-            // Prozessor anhalten
-            stopCPU();
-        }
-    } else {
-        // Hier den Hardwareinterrupt behandeln
-		if (cpu->intr >= 0x20 && cpu->intr <= 0x2f) {
-			if (cpu->intr >= 0x28) {
-				outb(0xa0, 0x20);
-			}
-			outb(0x20, 0x20);
-		}
-		terminal_writestring("Interrupt!");
-    }
-}
