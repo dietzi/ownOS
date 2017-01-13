24e750d01644c48a76e48b10c3f28501cf1e447f
dietzi <info@madi-services.de>
2017-01-13 12:56:54 +0100

    Code changed

--- kernel/multitasking.c
+++ kernel/multitasking.c
@@ -68,7 +68,7 @@ struct registers_t* init_task(uint8_t* stack, uint8_t* user_stack, void* entry)
 static uint32_t tss[32] = { 0, 0, 0x10 };
 static int current_task = -1;
 static int num_tasks = 2;
-static struct registers_t task_states[2];
+static struct registers_t* task_states[2];
  
 void init_multitasking(void)
 {
@@ -88,7 +88,7 @@ void init_multitasking(void)
  * Prozessorzustand wird als Parameter uebergeben und gespeichert, damit er
  * beim naechsten Aufruf des Tasks wiederhergestellt werden kann
  */
-struct registers_t* schedule(struct registers_t cpu)
+struct registers_t* schedule(struct registers_t* cpu)
 {
     /*
      * Wenn schon ein Task laeuft, Zustand sichern. Wenn nicht, springen wir
@@ -112,7 +112,7 @@ struct registers_t* schedule(struct registers_t cpu)
 
 struct registers_t* handle_multitasking(struct registers_t cpu)
 {
-    struct registers_t new_cpu = cpu;
+    struct registers_t* new_cpu = cpu;
     new_cpu = schedule(cpu);
 	//tss[1] = (uint32_t) (new_cpu + 1);
  
