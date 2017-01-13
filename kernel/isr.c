fba376d85ed98b644f854a8704be412d8c5a27c5
dietzi <info@madi-services.de>
2017-01-13 13:31:03 +0100

    Code changed

--- kernel/isr.c
+++ kernel/isr.c
@@ -29,19 +29,6 @@ void isr_handler(registers_t regs)
     }
 }
 
-void handle_interrupt(struct cpu_state* cpu)
-{
-	terminal_writestring('' + cpu->intr);
-	/*if (cpu->intr == 0x20) {
-		terminal_writestring("IRQ0");
-		handle_multitasking(cpu);
-	}*/
-	if (cpu->intr > 0x28) {
-		outb(SLAVE_COMMAND, PIC_RESET);
-	}
-	outb(MASTER_COMMAND, PIC_RESET);
-}
-
 void irq_handler(registers_t regs)
 {
 	if(regs.int_no != IRQ0 && regs.int_no != IRQ1) {
@@ -50,10 +37,26 @@ void irq_handler(registers_t regs)
 		itoa(regs.int_no,*result,10);
 		terminal_writestring(*result);
 	}
-	/*if(regs.int_no == IRQ0) {
+	if(regs.int_no == IRQ0) {
+		struct cpu_type* cpu;
+		cpu->eax = regs.eax;
+		cpu->ebx = regs.ebx;
+		cpu->ecx = regs.ecx;
+		cpu->edx = regs.edx;
+		cpu->esi = regs.esi;
+		cpu->edi = regs.edi;
+		cpu->ebp = regs.ebp;
+		cpu->intr = regs.int_no;
+		cpu->error = regs.err_code;
+		cpu->eip = regs.eip;
+		cpu->cs = regs.cs;
+		cpu->eflags = regs.eflags;
+		cpu->esp = regs.useresp;
+		cpu->ss = regs.ss;
+		
 		terminal_writestring("IRQ0");
-		handle_multitasking(regs);
-	}*/
+		handle_multitasking(cpu);
+	}
 	if(regs.int_no == IRQ1) {
 		kbd_irq_handler();
 	}
